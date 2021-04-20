68c82621a65fa86edea800eb0a09b5874816f17e
zamorai <35697970+zamorai@users.noreply.github.com>
2021-04-20 13:59:23 -0400

    fixes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Graph from './graph';
 import Header from './header';
 
 const nodes = [1,2,3,4,5,6,7,8];
+const connections = [[3,5],[3,4],[8,6],[8,7],[4,6],[7,5],[6,7],[4,5],[7,1],[2,6],[2,4],[5,1]]
 
 export default function App() {
   const[selected, setSelected] = useState(1);
   const[weights, setWeights] = useState([12,56,3,34,54,6,890,12,32,4,5,65])
+  const[graph, setGraph] = useState({})
 
-  console.log(weights)
-  
+  const buildGraph = () => {
+    var newGraph = {}
+    var node1, node2, weight;
+    for(var i = 0; i < 12; i++) {
+      node1 = connections[i][0]
+      node2 = connections[i][1]
+      weight = weights[i]
+      
+      addEdge(node1, node2, weight)
+    }
+    
+    function addEdge(node1, node2, value) {
+      if(node1 in newGraph) {
+        newGraph[node1].push([node2, value])
+      } else {
+        newGraph[node1] = [[node2, value]]
+      }
+      if(node2 in newGraph) {
+        newGraph[node2].push([node1, value])
+      } else {
+        newGraph[node2] = [[node1, value]]
+      }
+    }
+    setGraph(newGraph)
+  }
+
   const randomSelect = () => {
     //setSelected(Math.floor(Math.random()*10%8+1)) 
     var tempWeights = []
     for(var i = 0; i < weights.length; i++) {
       tempWeights.push(Math.floor(Math.random()*50 + 1))
     }
-
     setWeights(tempWeights)
+    buildGraph()
   }
 
   return (
